d78089efcc0290c38cdc9324ce9fb915d517eea5
GustavoBatistaDev <102866009+GustavoBatistaDev@users.noreply.github.com>
2022-11-03 17:08:36 -0300

    Update settings.py

--- freelaway/settings.py
+++ freelaway/settings.py
@@ -147,7 +147,7 @@ MESSAGE_TAGS = {
 # Email
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST_USER = 'dkssnowsnow@gmail.com'
-EMAIL_HOST_PASSWORD = 'wdpjqtlwlkrkkhye'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST = 'smtp.gmail.com'
